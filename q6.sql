488fe093a722a92c8e25a289986d96c01e6a4103
Matan Rafalovitz <matanraf@gmail.com>
2025-04-23 22:08:25 +0300

    more paranthesis

--- q6.sql
+++ q6.sql
@@ -1,5 +1,5 @@
 select name
-from  members natural join 
+from (members natural join 
 ((select number
 from members natural join (select number, uid
 from memberInKnesset
@@ -7,5 +7,5 @@ where party = 'Mapai')
 where name = 'David Ben Gurion') BenGurionKnessets natural join 
 (select number, uid
 from memberInKnesset
-where party = 'Mapai') MapaiMembers) MapaiBenGurionKnessets
+where party = 'Mapai') MapaiMembers) MapaiBenGurionKnessets) membersWithBenGurion
 order by name;
