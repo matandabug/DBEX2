79e11423b1822a00a2dfc584eb0470c83e8921a7
Matan Rafalovitz <matanraf@gmail.com>
2025-04-23 22:03:57 +0300

    gave alias q6

--- q6.sql
+++ q6.sql
@@ -7,5 +7,5 @@ where party = 'Mapai')
 where name = 'David Ben Gurion') natural join 
 (select number, uid
 from memberInKnesset
-where party = 'Mapai')
+where party = 'Mapai') MapaiMembers
 order by name;
