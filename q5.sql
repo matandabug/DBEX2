a506df3edc2d7ebc0a20f3b69f3d0a36cd023733
Matan Rafalovitz <matanraf@gmail.com>
2025-04-23 22:02:17 +0300

    gave alias

--- q5.sql
+++ q5.sql
@@ -6,6 +6,6 @@ from (select *
     from (select m1.uid as uid
         from memberInKnesset m1, memberInKnesset m2
         where m1.uid = m2.uid and m1.number <> m2.number) T natural join members
-)
+) T1
 where birthPlace = 'Jerusalem'
 order by name;
