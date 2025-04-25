3b68b3db3b7da96e1653a2b692e67018881af1cd
Matan Rafalovitz <matanraf@gmail.com>
2025-04-25 11:56:38 +0300

    should work?

--- q6.sql
+++ q6.sql
@@ -12,16 +12,11 @@
 -- order by name;
 
 select name
-from (select distinct * 
-from members 
-natural join (select number, uid
-from memberInKnesset
-where party = 'Mapai')) M
-where not exists ((select number
-from members natural join (select number, uid
-from memberInKnesset
-where party = 'Mapai') MapaiMembers1
-where name = 'David Ben-Gurion')
+from members M join memberInKnesset MIK on MIK.uid = M.uid
+where MIK.party = 'Mapai' and not exists (
+    (select number
+from members Mem1 join memberInKnesset MIK1 on MIK1.uid = Mem1.uid
+where Mem1.name = 'David Ben-Gurion' and MIK1.party = 'Mapai')
 except 
 (select number
 from memberInKnesset M1
