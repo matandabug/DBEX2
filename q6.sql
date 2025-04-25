be0cf1daf010c7b029db5a3e13d69f3e102d5751
Matan Rafalovitz <matanraf@gmail.com>
2025-04-25 11:19:58 +0300

    added dash

--- q6.sql
+++ q6.sql
@@ -1,22 +1,22 @@
--- select name
--- from (members natural join 
--- (select * 
--- from (select number
--- from members natural join (select number, uid
--- from memberInKnesset
--- where party = 'Mapai') MapaiMembers1
--- where name = 'David Ben Gurion') BenGurionKnessets natural join 
--- (select number, uid
--- from memberInKnesset
--- where party = 'Mapai') MapaiMembers) MapaiBenGurionKnessets) membersWithBenGurion
--- order by name;
+select name
+from (members natural join 
+(select * 
+from (select number
+from members natural join (select number, uid
+from memberInKnesset
+where party = 'Mapai') MapaiMembers1
+where name = 'David Ben-Gurion') BenGurionKnessets natural join 
+(select number, uid
+from memberInKnesset
+where party = 'Mapai') MapaiMembers) MapaiBenGurionKnessets) membersWithBenGurion
+order by name;
 
 -- select * 
 -- from (
-    select number, name
-from members natural join (select number, uid
-from memberInKnesset
-where party = 'Mapai') MapaiMembers1;
+--     select number, name
+-- from members natural join (select number, uid
+-- from memberInKnesset
+-- where party = 'Mapai') MapaiMembers1;
 -- where name = 'David Ben Gurion';
 -- )
 --  BenGurionKnessets natural join 
