51d7879c51fff9ed24cd42e6c5be7b6aa40fb14b
Matan Rafalovitz <matanraf@gmail.com>
2025-04-25 13:11:55 +0300

    changed s

--- q7.sql
+++ q7.sql
@@ -1,12 +1,12 @@
 select number as k, name as n
 from members natural join (
     select * 
-    from membersInKnesset
+    from memberInKnesset
     except
     select *
-    from membersInKnesset natural join (select m1.number as number, m1.uid as uid
-    from (membersInKnesset natural join (select uid, birthYear from members) m11) m1 JOIN
-    (membersInKnesset natural join (select uid, birthYear from members) m21) m2 on m1.number = m2.number
+    from memberInKnesset natural join (select m1.number as number, m1.uid as uid
+    from (memberInKnesset natural join (select uid, birthYear from members) m11) m1 JOIN
+    (memberInKnesset natural join (select uid, birthYear from members) m21) m2 on m1.number = m2.number
     where m1.birthYear > m2.birthYear) T1
 ) T2
 order by k, n;
