e640ba7354a27284d8fa0882a3d64f17d4a209b4
Matan Rafalovitz <matanraf@gmail.com>
2025-04-25 11:22:22 +0300

    check

--- q6.sql
+++ q6.sql
@@ -1,23 +1,23 @@
-select distinct name
-from (members natural join 
-(select * 
-from (select number
-from members natural join (select number, uid
-from memberInKnesset
-where party = 'Mapai') MapaiMembers1
-where name = 'David Ben-Gurion') BenGurionKnessets natural join 
-(select number, uid
-from memberInKnesset
-where party = 'Mapai') MapaiMembers) MapaiBenGurionKnessets) membersWithBenGurion
-order by name;
+-- select distinct name
+-- from (members natural join 
+-- (select * 
+-- from (select number
+-- from members natural join (select number, uid
+-- from memberInKnesset
+-- where party = 'Mapai') MapaiMembers1
+-- where name = 'David Ben-Gurion') BenGurionKnessets natural join 
+-- (select number, uid
+-- from memberInKnesset
+-- where party = 'Mapai') MapaiMembers) MapaiBenGurionKnessets) membersWithBenGurion
+-- order by name;
 
 -- select * 
 -- from (
---     select number, name
--- from members natural join (select number, uid
--- from memberInKnesset
--- where party = 'Mapai') MapaiMembers1;
--- where name = 'David Ben Gurion';
+    select number
+from members natural join (select number, uid
+from memberInKnesset
+where party = 'Mapai') MapaiMembers1;
+where name = 'David Ben-Gurion';
 -- )
 --  BenGurionKnessets natural join 
 -- (select number, uid
