4eb4e6ca0e5835e08d4caa69564f3981576e29f7
Matan Rafalovitz <matanraf@gmail.com>
2025-04-25 11:15:10 +0300

    took only single part

--- q6.sql
+++ q6.sql
@@ -11,12 +11,15 @@
 -- where party = 'Mapai') MapaiMembers) MapaiBenGurionKnessets) membersWithBenGurion
 -- order by name;
 
-select * 
-from (select number
+-- select * 
+-- from (
+    select number
 from members natural join (select number, uid
 from memberInKnesset
 where party = 'Mapai') MapaiMembers1
-where name = 'David Ben Gurion') BenGurionKnessets natural join 
-(select number, uid
-from memberInKnesset
-where party = 'Mapai') MapaiMembers;
+where name = 'David Ben Gurion'
+-- )
+--  BenGurionKnessets natural join 
+-- (select number, uid
+-- from memberInKnesset
+-- where party = 'Mapai') MapaiMembers;
