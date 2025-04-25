32ff4327d37f74c96dab475fa0b6379ae6f77c19
Matan Rafalovitz <matanraf@gmail.com>
2025-04-25 12:55:01 +0300

    removed comments from 6

--- q6.sql
+++ q6.sql
@@ -1,16 +1,3 @@
--- select distinct name
--- from (members natural join 
--- (select * 
--- from (select number
--- from members natural join (select number, uid
--- from memberInKnesset
--- where party = 'Mapai') MapaiMembers1
--- where name = 'David Ben-Gurion') BenGurionKnessets natural join 
--- (select number, uid
--- from memberInKnesset
--- where party = 'Mapai') MapaiMembers) MapaiBenGurionKnessets) membersWithBenGurion
--- order by name;
-
 select distinct name
 from (members M natural join memberInKnesset MIK) as MEMS
 where MEMS.party = 'Mapai' and name <> 'David Ben-Gurion' and not exists (
@@ -22,15 +9,3 @@ except
 from memberInKnesset M1
 where M1.uid = MEMS.uid))
 order by name;
-
--- select distinct name
--- from (select * from members M natural join memberInKnesset MIK) as MEMS
--- where MEMS.party = 'Mapai' and not exists (
---     (select number
--- from members Mem1 join memberInKnesset MIK1 on MIK1.uid = Mem1.uid
--- where Mem1.name = 'David Ben-Gurion' and MIK1.party = 'Mapai')
--- except 
--- (select number
--- from memberInKnesset M1
--- where M1.uid = MEMS.uid))
--- order by name;
