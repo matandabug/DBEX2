af18ff8047368d9830f19321fef6f89c6a278e3a
Matan Rafalovitz <matanraf@gmail.com>
2025-04-25 13:04:36 +0300

    added alias

--- q7.sql
+++ q7.sql
@@ -7,6 +7,6 @@ from members natural join (
     from membersInKnesset natural join (select m1.number as number, m1.uid as uid
     from (membersInKnesset natural join (select uid, birthYear from members) m11) m1 JOIN
     (membersInKnesset natural join (select uid, birthYear from members) m21) m2 on m1.number = m2.number
-    where m1.birthYear > m2.birthYear)
+    where m1.birthYear > m2.birthYear) T1
 )
 order by k, n;
