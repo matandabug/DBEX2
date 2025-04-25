20671f9986e7b18a0afd014df530da62fd01ccd5
Matan Rafalovitz <matanraf@gmail.com>
2025-04-25 12:59:36 +0300

    added aliasses

--- q7.sql
+++ q7.sql
@@ -5,8 +5,8 @@ from members natural join (
     except
     select *
     from membersInKnesset natural join (select m1.number as number, m1.uid as uid
-    from ((membersInKnesset natural join (select uid, birthYear from members)) m1 JOIN
-    (membersInKnesset natural join (select uid, birthYear from members)) m2)
+    from ((membersInKnesset natural join (select uid, birthYear from members) m11) m1 JOIN
+    (membersInKnesset natural join (select uid, birthYear from members) m21) m2)
     where m1.number = m2.number and m1.birthYear > m2.birthYear)
 )
 order by k, n;
