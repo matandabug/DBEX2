31ab5b8f356f0d224125abaab358886cc1c456f3
Matan Rafalovitz <matanraf@gmail.com>
2025-04-25 12:02:20 +0300

    asdf

--- q6.sql
+++ q6.sql
@@ -22,3 +22,15 @@ except
 from memberInKnesset M1
 where M1.uid = M.uid))
 order by name;
+
+select name
+from (select * from members M join memberInKnesset MIK on MIK.uid = M.uid) MEMS
+where MEMS.party = 'Mapai' and not exists (
+    (select number
+from members Mem1 join memberInKnesset MIK1 on MIK1.uid = Mem1.uid
+where Mem1.name = 'David Ben-Gurion' and MIK1.party = 'Mapai')
+except 
+(select number
+from memberInKnesset M1
+where M1.uid = MEMS.uid))
+order by name;
