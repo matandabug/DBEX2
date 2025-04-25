4dd0d82d0eecae184552309a852da4017dd0ef15
Matan Rafalovitz <matanraf@gmail.com>
2025-04-25 11:17:30 +0300

    no dbg check

--- q6.sql
+++ q6.sql
@@ -16,8 +16,8 @@
     select number
 from members natural join (select number, uid
 from memberInKnesset
-where party = 'Mapai') MapaiMembers1
-where name = 'David Ben Gurion'
+where party = 'Mapai') MapaiMembers1;
+-- where name = 'David Ben Gurion';
 -- )
 --  BenGurionKnessets natural join 
 -- (select number, uid
